857f8096f450a2ad145d6e0de70fb6e526a5a626
PacoCoca <pacococacruz@gmail.com>
2020-09-11 13:08:28 +0200

    delete test user

--- api_python/database/schema.sql
+++ api_python/database/schema.sql
@@ -23,12 +23,3 @@ CREATE TABLE `private` (
   `field1` TEXT,
   `field2` TEXT
 );
-
-/* test user, the password is bar */
-INSERT INTO `user`(`email`, `password`, `name`, `type`)
-  VALUES(
-    'foo@foo.foo',
-    'pbkdf2:sha256:150000$pPvRhOB6$cf2ead387415b8d3484940a81d5819460bff0ca8506c4211bd0e1f2dc72003e1',
-    'admin',
-    'admin'
-  );
